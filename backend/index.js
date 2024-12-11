5c564d989eeafa237ff8e6d4363d56b607d22e7a
Stanislaus  Nwankwo <stan@Stanislauss-MacBook-Pro.local>
2024-12-11 23:01:53 +0100

    connect backend to db

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const port = 3001;
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'mariadb-container',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'password',
+  password: process.env.DB_PASSWORD || 'yourpassword',
   database: process.env.DB_NAME || 'mydatabase'
 });
 
@@ -16,11 +16,15 @@ const connection = mysql.createConnection({
 // API endpoint
 app.get('/api', (req, res) => {
     connection.query('SELECT * FROM mytable', (error, results) => {
+        console.log("WE ARE HERE!!!")
         if (error) throw error;
         res.send(results);
     });
+    // res.send("ccccc")
 });
 
 app.listen(port, () => {
     console.log(`Backend API listening on this port ${port}`);
 });
+
+// docker run --name mariadb-container -e MARIADB_ROOT_PASSWORD=yourpassword -p 3306:3306 -d mariadb:10.6
